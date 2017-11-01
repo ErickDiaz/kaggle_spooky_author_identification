263f00d0580899394d076d94d57e3004dbb2d5fb
Erick Diaz <erick.diaz@xoom.com>
2017-11-01 08:23:08 -0600

    completed script

--- MWS_data_explorer.py
+++ MWS_data_explorer.py
@@ -1,6 +1,8 @@
 import pandas
 import os
 import sys
+import nltk
+from nltk.tokenize import sent_tokenize
 
 def validatePathName(pathName):
 	if(pathName.endswith('/')):
@@ -25,17 +27,33 @@ mwsCorpusPath= validatePathName(devmwsCorpusPath if ( len(sys.argv) < 2 ) else s
 files = os.listdir(mwsCorpusPath)
 
 mwsCorpus = ''
+raw_corpus = ''
 
 for file in files:
 	if (file.endswith('txt')):
 		print('|----------- ' + file)
-		with open((mwsCorpusPath+file), 'r') as f:
-				mwsCorpus+=f.read()
-		
-##TODO: use NLTK
+		openFile = open((mwsCorpusPath+file), 'r')
+		raw_corpus += openFile.read()
+
+mwsCorpus = sent_tokenize(raw_corpus.replace('\n', ' '))
+#text = nltk.Text(newData)
+
 for index, row in mwsData.iterrows():
-	text = row['text']
-	if text not in mwsCorpus:
-		print('------------ Not found -----------')
-		print(text)
-		print('----------------------------------')
+	kaggle_sentence = row['text']
+	found = False
+	for sentence in mwsCorpus:
+		result = nltk.edit_distance(sentence, kaggle_sentence)
+		if(result < 45):
+			print('kaggle sentence ----->' + kaggle_sentence +'<-------')
+			print('corpus: ------->' + sentence +'<-------')
+			found = True
+			mwsCorpus.remove(sentence)
+			break
+	if (not found):
+		print('#####################################################')
+		print('#################### NOT FOUND ######################')
+		print('kaggle sentence ----->' + kaggle_sentence +'<-------')
+		print('#####################################################')
+
+
+
